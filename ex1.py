4ddb958001a0e6fbc5121b08fcb38ef4d95022f3
anusha <anushakizhakkeveedu@gmail.com>
2023-10-30 09:58:21 +0530

    initial commit

---
+++ ex1.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print(3+2)
+print(5.2>=-2)
+
+formatter = "{} {} {} {}"
+print(formatter.format(1, 2, 3, 4))
+
+print(""" hello \n wolrd""")
+
+
+backslash_cat = "I'm \t a \ncat."
+print(backslash_cat)
+
