67566e08458724c848cfc19b3f6b71e0578f021c
anusha <anushakizhakkeveedu@gmail.com>
2023-11-26 10:37:21 +0530

    Add ex1 to ex7

--- ex13.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-
-script, first_argument, second_argument = argv
-
-print("The script is called:", script)
-print("Your first argument is:", first_argument)
-print("Your second argument is:", second_argument)
