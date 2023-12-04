ecd9ac4fa98cb09e74d599acac118abc035997dd
anusha <anushakizhakkeveedu@gmail.com>
2023-12-04 21:14:21 +0530

    Updated

--- ex17.py
+++ ex17.py
@@ -9,7 +9,7 @@ indata = in_file.read()
 
 print(f"The input file is {len(indata)} bytes long")
 
-1rint(f"Does the output file exist? {exists(to_file)}")
+print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
 out_file = open(to_file, 'w')
