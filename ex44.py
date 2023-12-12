ef203038430f4da881920e0f31497635a60da857
anusha <anushakizhakkeveedu@gmail.com>
2023-12-12 10:38:46 +0530

    Alter before or after

--- ex44.py
+++ ex44.py
@@ -23,3 +23,23 @@ son = Child()
 
 dad.override()
 son.override()
+
+#alter before or after 
+
+class Parent(object):
+
+    def altered(self):
+        print("PARENT altered()")
+
+class Child(Parent):
+
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        super(Child, self).altered()
+        print("CHILD, AFTER PARENT altered()")
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
