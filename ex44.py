3206b47a215a5b46c4d1cdf1ebeffc74447b3d39
anusha <anushakizhakkeveedu@gmail.com>
2023-12-12 10:35:40 +0530

    Add override explicity

--- ex44.py
+++ ex44.py
@@ -9,3 +9,17 @@ son = child()
 
 dad.implicit()
 son.implicit()
+
+
+#override explicity
+class Parent(object):
+    def override(self):
+        print("parent override()")
+class Child(Parent):
+    def override(self):
+        print("child override()")
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
